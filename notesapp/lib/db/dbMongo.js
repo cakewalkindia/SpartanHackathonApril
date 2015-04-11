9ab925d699ac4902a50ff7ef4a6490917b17430f
Asif Solkar <asif@cakewalk.in>
2015-04-11 15:40:48 +0530

    Structure Creation
    Structure created , all required packages added

---
+++ notesapp/lib/db/dbMongo.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Asif on 11-04-2015.
+ */
+dbMongo = ({
+    Notebook : new Mongo.Collection('notebook'),
+    Notes : new Mongo.Collection('notes'),
+    NotesTags  : new Mongo.Collection('notestags'),
+    NotesHistory : new Mongo.Collection('noteshistory'),
+    NotesAttachment : new Mongo.Collection('notesattachment'),
+    NotesShared : new Mongo.Collection('notesshared')
+})
+
+
+
---
+++ notesapp/routes/route.js
@@ -0,0 +1,7 @@
+/**
+ * Created by Asif on 11-04-2015.
+ */
+Router.map(function(){
+    this.route('home', {path: '/'} );
+
+});
