fc736c1d325a95cfc687f7ae467432fb636acff2
Asif Solkar <asif@cakewalk.in>
2015-04-11 17:34:33 +0530

    iron router error corrected

---
+++ notesapp/lib/db/collections/colNotebook.js
@@ -0,0 +1,15 @@
+/**
+ * Created by Asif on 11-04-2015.
+ */
+
+colNotebook = ({
+
+    createNotebook : function (_notebookname){
+
+    }, getDefaultnotebook : function(){
+
+    }
+
+
+
+});
--- notesapp/routes/route.js
+++ notesapp/routes/route.js
@@ -1,6 +1,10 @@
 /**
  * Created by Asif on 11-04-2015.
  */
+
+
+
+
 Router.map(function(){
     this.route('homeTemplate', {path: '/'} );
 
