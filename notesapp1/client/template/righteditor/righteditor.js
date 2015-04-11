a88c185ba8d05f36a1af50597d4ed0f6143565c1
Chetan bhimewal <chetan@cakewalk.in>
2015-04-12 01:41:41 +0530

    Revert "editor changes"
    This reverts commit 6434625b2d5b888f9a5697cf5e8523daba40e52a.

--- notesapp1/client/template/righteditor/righteditor.js
+++ notesapp1/client/template/righteditor/righteditor.js
@@ -1,30 +1,3 @@
-
-NotesAttachment=new FS.Collection('notesattachment',{
-    stores:[new FS.Store.FileSystem('notesattachment',{path:'C:/attachment'})]
-});
-
-
-if (Meteor.isClient) {
-
-    /*Template.hello.helpers({
-     notesattachment:function(){
-     return NotesAttachment.find();
-     }
-     });*/
-    Template.righteditorTemplate.rendered = function(){
-        $('#txteditor').wysihtml5();
-    };
-    Template.righteditorTemplate.events({
-
-        'change .fileInput':function(event,template){
-            FS.Utility.eachFile(event,function(file){
-                var fileObj=new FS.File(file);
-                /*NotesAttachment.insert({NoteId:'1', Name:fileObj.original,Attachment_Path:'C:/projectUploads',UserId:Meteor.userId()},function(err){
-                 console.log(err);*/
-                NotesAttachment.insert(fileObj,function(err){
-                    console.log(err);
-                })
-            })
-        }
-    });
-}
+/**
+ * Created by Asif on 11-04-2015.
+ */
--- notesapp1/server/dbCollection.js
+++
@@ -1,7 +0,0 @@
-/**
- * Created by chetan on 11-04-2015.
- */
-
-Attachments=new FS.Collection('notesattachment',{
-    stores:[new FS.Store.FileSystem('notesattachment',{path:'C:/attachment'})]
-});
