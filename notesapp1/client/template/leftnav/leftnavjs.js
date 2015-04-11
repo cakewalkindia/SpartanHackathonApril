54d75152c14d353de67f61fc0ee45e8f4c6cf7ec
shaishavgandhi05 <shaishavgandhi05@gmail.com>
2015-04-12 01:46:49 +0530

    Left Nav Overwrite

--- notesapp1/client/template/leftnav/leftnavjs.js
+++ notesapp1/client/template/leftnav/leftnavjs.js
@@ -7,6 +7,19 @@ if(Meteor.isClient) {
     Template.leftnavTemplate.events({
         "click #btnsubmit": function () {
             colNotebook.createNotebook("testing notebook create");
+        },
+
+        "click #createNotebook":function(event,tpl){
+            var name=tpl.find("#notebookName").value;
+            //var name = $('#notebookName')[0].value;
+            //
+            colNotebook.createNotebook(name);
+
+            // Clear form
+            $('#notebookName')[0].value = "";
+            $('#notebookModal').modal('hide');
+            // Prevent default form submit
+            return false;
         }
     });
 
