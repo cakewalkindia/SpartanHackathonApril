a88b4a14ada5777e618986c8fb565acab6c639d0
Chetan bhimewal <chetan@cakewalk.in>
2015-04-12 21:32:18 +0530

    history and delete note changes

--- notesapp1/client/template/modals/History/History.js
+++ notesapp1/client/template/modals/History/History.js
@@ -1,3 +1,34 @@
 /**
  * Created by SOHEB.RAPATI on 12-04-2015.
  */
+
+if(Meteor.isClient) {
+    Template.historytemplate.helpers({
+        historylist: function () {
+          return colHistory.getHistory();
+
+       } 
+    });
+
+
+
+    colHistory = ({
+        getHistory: function () {
+            var noteid=Session.get('noteid');
+            var divhistory=$('#divhistory');
+            if(noteid==null || noteid=="'"){
+                divhistory.hide();
+                return;
+            }
+            var notes = dbMongo.Notes.find({'_id' :Session.get('noteid')}).fetch()[0].history;
+            //divhistory.show();
+           // notes=notes.fetch();
+            return notes;
+        }
+
+        });
+
+    Template.historytemplate.rendered= function () {
+
+    }
+}
